c9001990b362f8210b652b7daac5a0751236b3dc
Mustafha24 <mustafha.ahmad24@gmail.com>
2024-03-31 14:49:17 +0530

    Chnaged theme

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import MessageContainer from "../components/messages/MessageContainer";
 
 const Home = () => {
   return (
-    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-blur-sm bg-opacity-0 border-2 border-amber-700 hover:border-stone-800 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-103'>
+    <div data-theme="sunset" className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-blur-sm bg-opacity-0 border-2 border-amber-700 hover:border-stone-800 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-103'>
       <Sidebar />
       <MessageContainer />
     </div>
