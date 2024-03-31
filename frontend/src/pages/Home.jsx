909abbd485b8d211ecfa8be9ba9be0e931919fe1
Mustafha24 <mustafha.ahmad24@gmail.com>
2024-03-31 16:09:57 +0530

    changed Styles for mobile view

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,9 +3,9 @@ import MessageContainer from "../components/messages/MessageContainer";
 
 const Home = () => {
   return (
-    <div data-theme="sunset" className='flex flex-col sm:flex-row sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-blur-sm bg-opacity-0 border-2 border-amber-700 hover:border-stone-800 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-103'>
-      <Sidebar className='sm:w-1/3' /> {/* Adjust width for sidebar on small screens */}
-      <MessageContainer className='sm:w-2/3' /> {/* Adjust width for message container on small screens */}
+    <div data-theme="sunset" className='flex sm:flex-row md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-blur-sm bg-opacity-0 border-2 border-amber-700 hover:border-stone-800 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-103'>
+      <Sidebar />
+      <MessageContainer />
     </div>
   );
 };
